8c3d6a493dc626f4b6d11edcf002f1ea13befcd3
Shane Tomlinson <stomlinson@mozilla.com>
2014-11-26 11:59:33 +0000

    Extract common authentication API functions to a common class, AuthenticationAPI

--- client/auth/lightbox/api.js
+++ client/auth/lightbox/api.js
@@ -6,29 +6,13 @@
 
 define([
   'p-promise',
-  'client/lib/constants',
+  'client/lib/object',
   'client/lib/options',
-  'client/lib/url',
+  '../api',
   './lightbox',
   './iframe_channel'
-], function (p, Constants, Options, Url, Lightbox, IFrameChannel) {
+], function (p, ObjectHelpers, Options, AuthenticationAPI, Lightbox, IFrameChannel) {
   'use strict';
-
-  function getLightboxSrc(host, page, clientId, state, scope,
-      redirectUri, email) {
-    var queryParams = {
-      client_id: clientId,
-      state: state,
-      scope: scope,
-      redirect_uri: redirectUri
-    };
-
-    if (email) {
-      queryParams.email = email;
-    }
-
-    return host + '/' + page + Url.objectToQueryString(queryParams);
-  }
 
   function getLightbox() {
     //jshint validthis: true
@@ -56,7 +40,7 @@ define([
 
       var lightbox = getLightbox.call(self);
 
-      var src = getLightboxSrc(self._fxaHost, page, self._clientId,
+      var src = self.getFxaUrl(self._fxaHost, page, self._clientId,
             config.state, config.scope, config.redirect_uri,
             config.force_email);
       lightbox.load(src);
@@ -90,28 +74,6 @@ define([
     });
   }
 
-  function authenticate(page, config) {
-    /*jshint validthis: true*/
-    var self = this;
-
-    return p().then(function () {
-      var requiredOptions = ['scope', 'state', 'redirect_uri'];
-      Options.checkRequired(requiredOptions, config);
-
-      return openLightbox.call(self, page, config);
-    })
-    .then(function (lightbox) {
-      return waitForAuthentication.call(self, lightbox);
-    })
-    .then(function (result) {
-      self.unload();
-      return result;
-    }, function (err) {
-      self.unload();
-      throw err;
-    });
-  }
-
   /**
    * Authenticate users with a lightbox
    *
@@ -119,55 +81,35 @@ define([
    * @constructor
    */
   function LightboxAPI(clientId, options) {
-    if (! clientId) {
-      throw new Error('clientId is required');
-    }
-    this._clientId = clientId;
+    AuthenticationAPI.call(this, clientId, options);
 
     options = options || {};
-    this._fxaHost = options.fxaHost || Constants.DEFAULT_FXA_HOST;
-    this._window = options.window || window;
     this._lightbox = options.lightbox;
     this._channel = options.channel;
   }
+  LightboxAPI.prototype = Object.create(AuthenticationAPI.prototype);
 
-  LightboxAPI.prototype = {
-    /**
-     * Sign in an existing user
-     *
-     * @method signIn
-     * @param {Object} config - configuration
-     *   @param {String} config.state
-     *   CSRF/State token
-     *   @param {String} config.redirect_uri
-     *   URI to redirect to when complete
-     *   @param {String} config.scope
-     *   OAuth scope
-     *   @param {String} [config.force_email]
-     *   Force the user to sign in with the given email
-     */
-    signIn: function (config) {
-      config = config || {};
-      var page = config.force_email ?
-                   Constants.FORCE_EMAIL_ENDPOINT :
-                   Constants.SIGNIN_ENDPOINT;
-      return authenticate.call(this, page, config);
-    },
+  ObjectHelpers.extend(LightboxAPI.prototype, {
+    authenticate: function (page, config) {
+      /*jshint validthis: true*/
+      var self = this;
 
-    /**
-     * Sign up a new user
-     *
-     * @method signUp
-     * @param {Object} config - configuration
-     *   @param {String} config.state
-     *   CSRF/State token
-     *   @param {String} config.redirect_uri
-     *   URI to redirect to when complete
-     *   @param {String} config.scope
-     *   OAuth scope
-     */
-    signUp: function (config) {
-      return authenticate.call(this, Constants.SIGNUP_ENDPOINT, config);
+      return p().then(function () {
+        var requiredOptions = ['scope', 'state', 'redirect_uri'];
+        Options.checkRequired(requiredOptions, config);
+
+        return openLightbox.call(self, page, config);
+      })
+      .then(function (lightbox) {
+        return waitForAuthentication.call(self, lightbox);
+      })
+      .then(function (result) {
+        self.unload();
+        return result;
+      }, function (err) {
+        self.unload();
+        throw err;
+      });
     },
 
     /**
@@ -186,7 +128,7 @@ define([
         self._channel.detach();
       });
     }
-  };
+  });
 
   return LightboxAPI;
 });
--- client/auth/redirect/api.js
+++ client/auth/redirect/api.js
@@ -3,39 +3,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 define([
+  '../api',
   'client/lib/constants',
   'client/lib/options',
-  'client/lib/url'
-], function (Constants, Options, Url) {
-
-  function getFxaUrl(host, page, clientId, state, scope,
-      redirectUri, email) {
-    var queryParams = {
-      client_id: clientId,
-      state: state,
-      scope: scope,
-      redirect_uri: redirectUri
-    };
-
-    if (email) {
-      queryParams.email = email;
-    }
-
-    return host + '/' + page + Url.objectToQueryString(queryParams);
-  }
-
-  function authenticate(page, config) {
-    var requiredOptions = ['scope', 'state', 'redirect_uri'];
-    Options.checkRequired(requiredOptions, config);
-
-    var self = this;
-    var fxaUrl = getFxaUrl(self._fxaHost, page, self._clientId,
-          config.state, config.scope, config.redirect_uri,
-          config.force_email);
-
-
-    this._window.location.href = fxaUrl;
-  }
+  'client/lib/object'
+], function (AuthenticationAPI, Constants, Options, ObjectHelpers) {
+  'use strict';
 
   /**
    * Authenticate a user with the redirect flow.
@@ -44,55 +17,25 @@ define([
    * @constructor
    */
   function RedirectAPI(clientId, options) {
-    if (! clientId) {
-      throw new Error('clientId is required');
-    }
-    this._clientId = clientId;
-
-    options = options || {};
-    this._fxaHost = options.fxaHost || Constants.DEFAULT_FXA_HOST;
-    this._window = options.window || window;
+    AuthenticationAPI.call(this, clientId, options);
   }
 
-  RedirectAPI.prototype = {
-    /**
-     * Sign in an existing user
-     *
-     * @method signIn
-     * @param {Object} config - configuration
-     *   @param {String} config.state
-     *   CSRF/State token
-     *   @param {String} config.redirect_uri
-     *   URI to redirect to when complete
-     *   @param {String} config.scope
-     *   OAuth scope
-     *   @param {String} [config.force_email]
-     *   Force the user to sign in with the given email
-     */
-    signIn: function (config) {
-      config = config || {};
-      var page = config.force_email ?
-                   Constants.FORCE_EMAIL_ENDPOINT :
-                   Constants.SIGNIN_ENDPOINT;
-      return authenticate.call(this, page, config);
-    },
+  RedirectAPI.prototype = Object.create(AuthenticationAPI.prototype);
+  ObjectHelpers.extend(RedirectAPI.prototype, {
+    authenticate: function (page, config) {
+      var requiredOptions = ['scope', 'state', 'redirect_uri'];
+      Options.checkRequired(requiredOptions, config);
 
-    /**
-     * Sign up a new user
-     *
-     * @method signUp
-     * @param {Object} config - configuration
-     *   @param {String} config.state
-     *   CSRF/State token
-     *   @param {String} config.redirect_uri
-     *   URI to redirect to when complete
-     *   @param {String} config.scope
-     *   OAuth scope
-     */
-    signUp: function (config) {
-      return authenticate.call(this, Constants.SIGNUP_ENDPOINT, config);
+      var self = this;
+      var fxaUrl = self.getFxaUrl(self._fxaHost, page, self._clientId,
+            config.state, config.scope, config.redirect_uri,
+            config.force_email);
+
+
+      this._window.location.href = fxaUrl;
     }
-  };
+
+  });
 
   return RedirectAPI;
 });
