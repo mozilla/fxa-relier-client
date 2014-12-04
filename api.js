ae8dd025fdb254a2354a4ca569e1d5eedcdb5c8d
Shane Tomlinson <stomlinson@mozilla.com>
2014-12-04 15:43:41 -0800

    Built fxa-relier-client from commit a80a809 on branch master

---
+++ api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "FxaRelierClient",
+        "LightboxAPI",
+        "RedirectAPI"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
