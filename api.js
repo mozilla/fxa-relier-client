3fe50d061103d58fb33c9bf78e54a092356411ef
Shane Tomlinson <stomlinson@mozilla.com>
2015-01-21 11:58:37 +0000

    Built fxa-relier-client from commit 0ec1e4b on branch master

--- api.js
+++ api.js
@@ -1,12 +1,31 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "AuthenticationAPI",
-        "FxaRelierClient",
-        "LightboxAPI",
-        "RedirectAPI"
+        "AuthAPI",
+        "BaseBroker",
+        "Constants",
+        "Function",
+        "FxaRelierClient (start here)",
+        "IFrameChannel",
+        "Lightbox",
+        "LightboxBroker",
+        "Object",
+        "Options",
+        "ProfileAPI",
+        "RedirectBroker",
+        "TokenAPI",
+        "Url",
+        "Xhr"
     ],
-    "modules": [],
-    "allModules": []
+    "modules": [
+        "FxaRelierClient"
+    ],
+    "allModules": [
+        {
+            "displayName": "FxaRelierClient",
+            "name": "FxaRelierClient",
+            "description": "The Firefox Accounts Relier Client."
+        }
+    ]
 } };
 });
